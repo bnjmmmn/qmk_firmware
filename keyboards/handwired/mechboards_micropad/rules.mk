f8987de31541f1953d5382db28ecd6610c2e249d
bnjmmmn <132272862+bnjmmmn@users.noreply.github.com>
2023-05-01 14:31:09 +0100

    Update rules.mk

--- keyboards/handwired/mechboards_micropad/rules.mk
+++ keyboards/handwired/mechboards_micropad/rules.mk
@@ -1,7 +1,7 @@
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
+BOOTMAGIC_ENABLE = yes       # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no        # Console for debug
@@ -10,3 +10,4 @@ NKRO_ENABLE = no            # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
+VIA_ENABLE = yes
